d2b99f608a1a1b771dd125a39d2095b76b2bb242
aoubak <aoubak01@gmail.com>
2025-08-04 01:13:02 +0530

    modified:   src/pages/ReviewPage.jsx

--- src/pages/ReviewPage.jsx
+++ src/pages/ReviewPage.jsx
@@ -3,8 +3,8 @@ import Reviews from "../components/Reviews";
 
 function ReviewPage() {
   return (
-    <section className="">
-      <div className="container mx-auto px-4 py-8">
+  
+      <div className="container mx-auto ">
         <div className="text-center bg-gradient-tob from-cyan-900/60  pt-30">
           <h2 className="text-2xl font-bold">Reviews</h2>
           <p className="mt-4 text-gray-300">
@@ -14,7 +14,7 @@ function ReviewPage() {
 
         <Reviews />
       </div>
-    </section>
+   
   );
 }
 
